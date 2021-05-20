bb79b2c089ddccafc046f1576b5db1c3630d995a
marco-amorim <marcoamorim95@hotmail.com>
2021-05-19 21:07:24 -0300

    replace requestmapping with getmapping

--- src/main/java/com/in28minutes/rest/webservices/restwebservices/controller/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restwebservices/controller/HelloWorldController.java
@@ -1,13 +1,12 @@
 package com.in28minutes.rest.webservices.restwebservices.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
-	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	@GetMapping(value = "/hello_world")
 	public String sayHelloWorld() {
 		return "Hello World!";
 	}
