33dff37e262a7071d6d0c69eae2b34589bafee38
marco-amorim <marcoamorim95@hotmail.com>
2021-05-20 18:34:32 -0300

    refactor user dao service dependency to use autowired

--- src/main/java/com/in28minutes/rest/webservices/restwebservices/controller/UserController.java
+++ src/main/java/com/in28minutes/rest/webservices/restwebservices/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.in28minutes.rest.webservices.restwebservices.dao.UserDaoService;
 import com.in28minutes.rest.webservices.restwebservices.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,17 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+	@Autowired
+	UserDaoService userDaoService;
+
 	@GetMapping("/users")
 	public List<User> getAllUsers() {
-		UserDaoService userDaoService = new UserDaoService();
-
 		return userDaoService.findAll();
 	}
 
 	@GetMapping("/users/{id}")
 	public User getUserById(@PathVariable int id) {
-		UserDaoService userDaoService = new UserDaoService();
-
 		return userDaoService.finOne(id);
 	}
 }
