bd0c575bb50266e8f5b03f35c95fcc96be088e4e
marco-amorim <marcoamorim95@hotmail.com>
2021-05-20 18:45:35 -0300

    created api to save user

--- src/main/java/com/in28minutes/rest/webservices/restwebservices/controller/UserController.java
+++ src/main/java/com/in28minutes/rest/webservices/restwebservices/controller/UserController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import com.in28minutes.rest.webservices.restwebservices.dao.UserDaoService;
 import com.in28minutes.rest.webservices.restwebservices.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,5 +26,12 @@ public class UserController {
 	@GetMapping("/users/{id}")
 	public User getUserById(@PathVariable int id) {
 		return userDaoService.finOne(id);
+	}
+
+	@PostMapping("/users")
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		User savedUser = userDaoService.save(user);
+
+		return ResponseEntity.status(201).body(savedUser);
 	}
 }
