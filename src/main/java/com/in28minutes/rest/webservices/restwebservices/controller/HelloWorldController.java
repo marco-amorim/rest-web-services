d6b6f9cec4a52c9f07f4cae6f03ab36775497200
marco-amorim <marcoamorim95@hotmail.com>
2021-06-04 21:20:20 -0300

    implemented i18n on hello world service

--- src/main/java/com/in28minutes/rest/webservices/restwebservices/controller/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restwebservices/controller/HelloWorldController.java
@@ -1,12 +1,20 @@
 package com.in28minutes.rest.webservices.restwebservices.controller;
 
+import java.util.Locale;
+
 import com.in28minutes.rest.webservices.restwebservices.model.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+	@Autowired
+	private MessageSource messageSource;
 
 	@GetMapping(value = "/hello-world")
 	public String helloWorld() {
@@ -24,7 +32,7 @@ public class HelloWorldController {
 	}
 
 	@GetMapping(value = "/hello-world-i18n")
-	public String helloWorldInternationalized() {
-		return "Hello World!";
+	public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+		return messageSource.getMessage("good.morning.message", null, locale);
 	}
 }
